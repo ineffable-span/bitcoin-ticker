a2a403806c01fd0a8947d06c953bc05d05600cfc
m-zaink <sidsmith9@gmail.com>
2019-06-05 22:46:44 +0530

    Extract ConversionCard widget for readability

---
+++ lib/components/conversion_card.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class ConversionCard extends StatelessWidget {
+
+  final String conversionPrice;
+  final String selectedCurrency;
+  final String coinType;
+
+  ConversionCard({this.coinType, this.conversionPrice, this.selectedCurrency});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+      child: Card(
+        color: Colors.lightBlueAccent,
+        elevation: 5.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+          child: Text(
+            '1 $coinType = $conversionPrice $selectedCurrency',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
